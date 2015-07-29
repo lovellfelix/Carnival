5a00139b013c9ee29d7153ce61838b96f8fbf4f4
Lovell Felix <lovell.felix@gmail.com>
2015-07-29 05:46:53 -0500

    Added post module

---
+++ modules/posts/client/config/posts.client.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Configuring the Posts module
+angular.module('posts').run(['Menus',
+	function(Menus) {
+		// Add the Posts dropdown item
+		Menus.addMenuItem('topbar', {
+			title: 'Posts',
+			state: 'posts',
+			type: 'dropdown'
+		});
+
+		// Add the dropdown list item
+		Menus.addSubMenuItem('topbar', 'posts', {
+			title: 'List Posts',
+			state: 'posts.list'
+		});
+
+		// Add the dropdown create item
+		Menus.addSubMenuItem('topbar', 'posts', {
+			title: 'Create Post',
+			state: 'posts.create'
+		});
+	}
+]);
---
+++ modules/posts/server/routes/posts.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	var posts = require('../controllers/posts.server.controller');
+	var postsPolicy = require('../policies/posts.server.policy');
+
+	// Posts Routes
+	app.route('/api/posts').all()
+		.get(posts.list).all(postsPolicy.isAllowed)
+		.post(posts.create);
+
+	app.route('/api/posts/:postId').all(postsPolicy.isAllowed)
+		.get(posts.read)
+		.put(posts.update)
+		.delete(posts.delete);
+
+	// Finish by binding the Post middleware
+	app.param('postId', posts.postByID);
+};
