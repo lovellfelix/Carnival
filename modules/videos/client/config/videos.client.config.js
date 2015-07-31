13dcbd12b0ad9b30f38c3a5de6b285d2618c8572
Lovell Felix <lovell.felix@gmail.com>
2015-07-31 01:37:29 -0500

    Added Video module

---
+++ modules/videos/client/config/videos.client.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Configuring the Videos module
+angular.module('videos').run(['Menus',
+    function (Menus) {
+        // Add the Video dropdown item
+        Menus.addMenuItem('topbar', {
+            title: 'Videos',
+            isPublic: true,
+            state: 'videos.slick',
+            icon: 'facetime-video',
+            position: 3
+        });
+    }
+]);
