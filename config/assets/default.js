b73706e205d6775980d3751b9c9eff0652ff2047
Lovell Felix <lovell.felix@gmail.com>
2015-08-10 08:05:09 -0500

    WIP: Initial work grid layout with mansory lib

--- config/assets/default.js
+++ config/assets/default.js
@@ -9,7 +9,19 @@ module.exports = {
 				'public/lib/angular-material/angular-material.css'
 			],
 			js: [
-				'public/lib/angular/angular.js',
+        'public/lib/jquery/dist/jquery.js',
+        'public/lib/jquery-bridget/jquery.bridget.js',
+        'publc/lib/get-style-property/get-style-property.js',
+        'publc/lib/get-size/get-size.js',
+        'publc/lib/eventEmitter/EventEmitter.js',
+        'publc/lib/eventie/eventie.js',
+        'publc/lib/doc-ready/doc-ready.js',
+        'publc/lib/matches-selector/matches-selector.js',
+        'publc/lib/outlayer/item.js',
+        'publc/lib/outlayer/outlayer.js',
+        'public/lib/masonry/dist/masonry.pkgd.min.js',
+        'public/lib/imagesloaded/imagesloaded.pkgd.min.js',
+        'public/lib/angular/angular.js',
         'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
@@ -21,7 +33,8 @@ module.exports = {
 				'public/lib/angular-gravatar/build/angular-gravatar.js',
 				'public/lib/angular-timeago/dist/angular-timeago.js',
         'public/lib/angular-img-fallback/angular.dcb-img-fallback.js',
-				'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js'
+				'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js',
+        'public/lib/angular-masonry/angular-masonry.js'
 			],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
--- config/assets/production.js
+++ config/assets/production.js
@@ -9,6 +9,18 @@ module.exports = {
 				'public/lib/angular-material/angular-material.min.css'
 			],
 			js: [
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/jquery-bridget/jquery.bridget.js',
+				'publc/lib/get-style-property/get-style-property.js',
+				'publc/lib/get-size/get-size.js',
+				'publc/lib/eventEmitter/EventEmitter.min.js',
+				'publc/lib/eventie/eventie.js',
+				'publc/lib/doc-ready/doc-ready.js',
+				'publc/lib/matches-selector/matches-selector.js',
+				'publc/lib/outlayer/item.js',
+				'publc/lib/outlayer/outlayer.js',
+				'public/lib/masonry/dist/masonry.pkgd.min.js',
+				'public/lib/imagesloaded/imagesloaded.pkgd.min.js',
 				'public/lib/angular/angular.min.js',
 				'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.min.js',
@@ -21,7 +33,8 @@ module.exports = {
 				'public/lib/angular-gravatar/build/angular-gravatar.min.js',
 				'public/lib/angular-timeago/dist/angular-timeago.min.js',
 				'public/lib/angular-img-fallback/angular.dcb-img-fallback.min.js',
-				'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js'
+				'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js',
+				'public/lib/angular-masonry/angular-masonry.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
