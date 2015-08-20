797789118135f229f8144cd3a74fc1163e9824a1
Lovell Felix <lovell.felix@gmail.com>
2015-08-19 22:17:49 -0500

    Fixed path issue

--- modules/posts/client/config/posts.client.routes.js
+++ modules/posts/client/config/posts.client.routes.js
@@ -7,7 +7,7 @@ angular.module('posts').config(['$stateProvider',
 		$stateProvider.
 		state('home', {
 			url: '/',
-			templateUrl: 'modules/posts/views/stream.client.view.html'
+			templateUrl: 'modules/posts/client/views/stream.client.view.html'
 		}).
 		state('posts', {
 			abstract: true,
@@ -16,19 +16,19 @@ angular.module('posts').config(['$stateProvider',
 		}).
 		state('posts.list', {
 			url: '',
-			templateUrl: 'modules/posts/views/list-posts.client.view.html'
+			templateUrl: 'modules/posts/client/views/list-posts.client.view.html'
 		}).
 		state('posts.create', {
 			url: '/create',
-			templateUrl: 'modules/posts/views/create-post.client.view.html'
+			templateUrl: 'modules/posts/client/views/create-post.client.view.html'
 		}).
 		state('posts.view', {
 			url: '/:postId',
-			templateUrl: 'modules/posts/views/view-post.client.view.html'
+			templateUrl: 'modules/posts/client/views/view-post.client.view.html'
 		}).
 		state('posts.edit', {
 			url: '/:postId/edit',
-			templateUrl: 'modules/posts/views/edit-post.client.view.html'
+			templateUrl: 'modules/posts/client/views/edit-post.client.view.html'
 		});
 	}
 ]);
