224d37eb2fcd34fe7411ca4f36716036854f0872
Lovell Felix <lovell.felix@gmail.com>
2015-08-29 19:38:25 -0500

    Added nycarnival

--- config/env/production.js
+++ config/env/production.js
@@ -71,7 +71,7 @@ module.exports = {
 	seedDB: process.env.MONGO_SEED || false,
 	lookupTwitterStream: true,
 	sendStats: true,
-	htag: 'spicemas, jabjab, cropover, jouvert, caribana, mystcarnival, grenadacarnival, trinidadcarnival, miamicarnival, nottinghillcarnival, bostoncarnival',
+	htag: 'spicemas, jabjab, cropover, jouvert, caribana, mystcarnival, grenadacarnival, trinidadcarnival, miamicarnival, nottinghillcarnival, bostoncarnival, nycarnival',
 	dates: {
 			start: '27/07/2015 00:00:00',
 			end: '22/08/2015 00:00:00'
