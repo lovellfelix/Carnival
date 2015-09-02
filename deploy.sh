04484cdf312303c73728d2a6bca9ff346ae9c10c
Lovell Felix <lovell.felix@gmail.com>
2015-09-02 07:53:15 -0500

    Fixed: fatal: empty ident name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 grunt build
+git config user.name ci
+git config user.email ci@lovellfelix.com
 git commit -am "Building for deployment"
 git remote add production dokku@ny.arawak.space:carnival
 git push production master -f
